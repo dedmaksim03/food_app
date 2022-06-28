c337b52e78fbf85612a17bd31d9ff363558acc35
dedmaksim03 <89924979+dedmaksim03@users.noreply.github.com>
2022-06-28 15:13:08 +0300

    Сделал крутящийся progress_bar

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.food_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/example/food_app/splash_screen.java
+++ app/src/main/java/com/example/food_app/splash_screen.java
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.window.SplashScreen;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splash_screen extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 2500;
-
+    private final int SPLASH_DISPLAY_LENGTH = 3500;
+    ImageView progress_bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progress_bar = findViewById(R.id.progress_bar);
+
+        final Animation progress_bar_anim = AnimationUtils.loadAnimation(this, R.anim.progress_bar_anim);
+        progress_bar.startAnimation(progress_bar_anim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
